32499c37e52be7a2c36995003162ed724c557a72
WzxKB <2461028063@qq.com>
2022-05-02 14:01:10 +0800

    3

--- php/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername="localhost";
-$sqlpwd="246102";
-$sqlname="root";
-$dbanme="devil-wzx";
-$conn=new mysqli($servername,$sqlname,$sqlpwd,$dbanme)or die("链接数据库失败".mysql_error());
-header('Content-Type: application/json');//加了这句话就后面不用再写解析JSON
-?>
